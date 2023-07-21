7cebc61238c57924ac71277e5a1edf031bddca57
Mahsak89 <khoshnoudmahsa@gmail.com>
2023-07-21 13:23:05 +0000

    Deployment Commit

--- coaching/settings.py
+++ coaching/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY =  os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["coaching-f4c8010fb34d.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ['coaching-f4c8010fb34d.herokuapp.com' , 'localhost']
+
 
 
 # Application definition
